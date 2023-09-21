95ae7726e9ca1f1267131113774e70e75c1733ad
Alexandra Vlasceanu <alexandra.vlasceanuuu@gmail.com>
2023-09-21 15:33:32 +0200

    Selecting, Creating and Deleting Elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,10 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+// for (let i = 0; i < btnsOpenModal.length; i++)    /// ^^^^
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +32,54 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// / Lessons ////
+
+// HOW DOM WORKS ///
+// DOM is the interface between all JavaScript code and the browser, or more specifically HTML documents that are rendered in
+// and by the browser
+// With DOM we can:
+// - we can use it to make JS interact with the browser
+// - we can create, modify and delete elements, set styles and classes and attributes and listen and respond  to events
+// - DOM tree is generated form any HTML document
+// - A DOM tree is a tree like structure made out of nodes
+// DOM API is broken up into different node types. Each types of nodes has access to different properties and methods and some
+// of them even inherit more properties and methods form their ancestors in this organisation
+
+/// SELECTING, CREATING AND DELETING ELEMENTS ///
+
+// Selecting elements //
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting documents //
+// .insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class ="btn btn--close--cookie">Got it!</button>';
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+// header.before(message);
+// header.after(message);
+
+// Delete elements //
+document
+  .querySelector('.btn--close--cookie')
+  .addEventListener('click', function () {
+    // message.remove();
+    message.parentElement.removeChild(message);
+  });
