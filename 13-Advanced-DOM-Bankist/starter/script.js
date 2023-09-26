364c332ef76af25c6396157dd83ef55f222eda8b
Alexandra Vlasceanu <alexandra.vlasceanuuu@gmail.com>
2023-09-26 09:50:27 +0200

    Styles, Attributes and Classes

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -83,3 +83,54 @@ document
     // message.remove();
     message.parentElement.removeChild(message);
   });
+///////////////////////// CLOSE ////////////////////////////////
+
+/// Styles, Attributes and Classes ///
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+console.log(message.style.color);
+console.log(message.style.background);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+// we want to add 40px to height
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+/// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+// set attributes
+logo.alt = 'Beautiful minimalist logo';
+logo.setAttribute('company', 'Bankist');
+
+// Non standard (get attributes)
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.remove('c');
+
+// Don't do it
+logo.className = 'jonas';
+
+//// IMPLEMENTING SMOOTH SCROLLING /////
